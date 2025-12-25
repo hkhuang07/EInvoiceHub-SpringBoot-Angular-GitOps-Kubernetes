e81ddbfbf1406d32e2f68f717737a2e45f55a3c0
Huynh Quoc Huy <huykyunh.k@gmail.com>
2025-12-25 11:50:59 +0700

    First Commit

---
+++ src/main/java/com/einvoicehub/core/EinvoiceCoreApplication.java
@@ -0,0 +1,13 @@
+package com.einvoicehub.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EinvoiceCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EinvoiceCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/einvoicehub/core/EinvoiceCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.einvoicehub.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EinvoiceCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
